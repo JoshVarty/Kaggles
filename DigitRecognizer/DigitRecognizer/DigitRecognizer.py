8f60bec7d7b0eb09559dad44e52ef89031dc8cb3
Josh Varty <joshvarty@gmail.com>
2017-08-23 14:02:20 -0600

    Revert "Adding dropout"
    This reverts commit 8d6383408490a86f160e6cf8cd17727c1184af89.

--- DigitRecognizer/DigitRecognizer/DigitRecognizer.py
+++ DigitRecognizer/DigitRecognizer/DigitRecognizer.py
@@ -76,26 +76,23 @@ def ConvNet():
       layer4_biases = tf.Variable(tf.constant(1.0, shape=[num_labels]))
 
       # Model.
-      def model(data, keep_prob):
+      def model(data):
         conv = tf.nn.conv2d(data, layer1_weights, [1, 1, 1, 1], padding='SAME')
         hidden = tf.nn.relu(conv + layer1_biases)
-        hidden = tf.nn.dropout(hidden, keep_prob)
         pool_1 = tf.nn.max_pool(hidden, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
         conv = tf.nn.conv2d(pool_1, layer2_weights, [1, 1, 1, 1], padding='SAME')
         hidden = tf.nn.relu(conv + layer2_biases)
-        hidden = tf.nn.dropout(hidden, keep_prob)
         pool_1 = tf.nn.max_pool(hidden, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
         shape = pool_1.get_shape().as_list()
         reshape = tf.reshape(pool_1, [shape[0], shape[1] * shape[2] * shape[3]])
         hidden = tf.nn.relu(tf.matmul(reshape, layer3_weights) + layer3_biases)
-        hidden = tf.nn.dropout(hidden, keep_prob)
         return tf.matmul(hidden, layer4_weights) + layer4_biases
 
       def accuracy(predictions, labels):
         return 100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1)) / predictions.shape[0]
       
       # Training computation.
-      logits = model(tf_train_dataset, 0.75)
+      logits = model(tf_train_dataset)
       loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits))
         
       # Optimizer.
@@ -103,7 +100,7 @@ def ConvNet():
       
       # Predictions for the training, validation, and test data.
       train_prediction = tf.nn.softmax(logits)
-      test_prediction = tf.nn.softmax(model(tf_test_dataset, 1.0))
+      test_prediction = tf.nn.softmax(model(tf_test_dataset))
 
       num_steps = 10001
 
