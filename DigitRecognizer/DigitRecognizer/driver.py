9d576c8cdadb2e937adc28c51eecf3069613cc76
Josh Varty <joshvarty@gmail.com>
2017-09-06 10:07:14 -0400

    Set up training of two models

--- DigitRecognizer/DigitRecognizer/driver.py
+++ DigitRecognizer/DigitRecognizer/driver.py
@@ -7,5 +7,13 @@ model_save_path3 = "model/model3.ckpt"
 model_save_path4 = "model/model4.ckpt" 
 model_save_path5 = "model/model5.ckpt" 
 
+#Three of the first model
 model1.TrainConvNet(model_save_path1)
-model2.TrainConvNet(model_save_path2);
+model1.TrainConvNet(model_save_path2)
+model1.TrainConvNet(model_save_path3)
+
+#Four of the second model
+model2.TrainConvNet(model_save_path4);
+model2.TrainConvNet(model_save_path5);
+model2.TrainConvNet(model_save_path6);
+model2.TrainConvNet(model_save_path7);
