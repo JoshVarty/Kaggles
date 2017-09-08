3c1ccf2fd9bf2993baef4b5272ab5b1e3021210a
Josh Varty <joshvarty@gmail.com>
2017-09-08 13:59:14 -0400

    Setup for third model

--- DigitRecognizer/DigitRecognizer/driver.py
+++ DigitRecognizer/DigitRecognizer/driver.py
@@ -1,5 +1,6 @@
 import model1
 import model2
+import model3
 import numpy as np
 
 model_save_path1 = "model/model.ckpt" 
@@ -10,52 +11,54 @@ model_save_path5 = "model/model5.ckpt"
 model_save_path6 = "model/model6.ckpt" 
 model_save_path7 = "model/model7.ckpt" 
 
+#Two of the third model
+model3.TrainConvNet(model_save_path6)
+model3.TrainConvNet(model_save_path7)
+
 ##Three of the first model
-#model1.TrainConvNet(model_save_path1)
-#model1.TrainConvNet(model_save_path2)
-#model1.TrainConvNet(model_save_path3)
+model1.TrainConvNet(model_save_path1)
+model1.TrainConvNet(model_save_path2)
+model1.TrainConvNet(model_save_path3)
 
-##Four of the second model
-#model2.TrainConvNet(model_save_path4);
-#model2.TrainConvNet(model_save_path5);
-#model2.TrainConvNet(model_save_path6);
-#model2.TrainConvNet(model_save_path7);
+#Two of the second model
+model2.TrainConvNet(model_save_path4)
+model2.TrainConvNet(model_save_path5)
 
 
-results1 = model1.LoadAndRun(model_save_path1)
-results2 = model1.LoadAndRun(model_save_path2)
-results3 = model1.LoadAndRun(model_save_path3)
-results4 = model1.LoadAndRun(model_save_path4)
+#results1 = model1.LoadAndRun(model_save_path1)
+#results2 = model1.LoadAndRun(model_save_path2)
+#results3 = model1.LoadAndRun(model_save_path3)
+#results4 = model1.LoadAndRun(model_save_path4)
 
-results5 = model2.LoadAndRun(model_save_path5)
-results6 = model2.LoadAndRun(model_save_path6)
-results7 = model2.LoadAndRun(model_save_path7)
+#results5 = model2.LoadAndRun(model_save_path5)
+#results6 = model2.LoadAndRun(model_save_path6)
+#results7 = model2.LoadAndRun(model_save_path7)
 
 
-with open("results/results.csv", 'w') as file:
-    file.write("ImageId,Label\n")
-    for idx in range(len(results1)):
-         pred1 = int(results1[idx])
-         pred2 = int(results2[idx])
-         pred3 = int(results3[idx])
-         pred4 = int(results4[idx])
-         pred5 = int(results5[idx])
-         pred6 = int(results6[idx])
-         pred7 = int(results7[idx])
+#with open("results/results.csv", 'w') as file:
+#    file.write("ImageId,Label\n")
+#    for idx in range(len(results1)):
+#         pred1 = int(results1[idx])
+#         pred2 = int(results2[idx])
+#         pred3 = int(results3[idx])
+#         pred4 = int(results4[idx])
+#         pred5 = int(results5[idx])
+#         pred6 = int(results6[idx])
+#         pred7 = int(results7[idx])
 
-         nums = np.zeros(10)
+#         nums = np.zeros(10)
          
-         nums[pred1] = nums[pred1] + 1 
-         nums[pred2] = nums[pred2] + 1 
-         nums[pred3] = nums[pred3] + 1 
-         nums[pred4] = nums[pred4] + 1 
-         nums[pred5] = nums[pred5] + 1 
-         nums[pred6] = nums[pred6] + 1 
-         nums[pred7] = nums[pred7] + 1 
+#         nums[pred1] = nums[pred1] + 1 
+#         nums[pred2] = nums[pred2] + 1 
+#         nums[pred3] = nums[pred3] + 1 
+#         nums[pred4] = nums[pred4] + 1 
+#         nums[pred5] = nums[pred5] + 1 
+#         nums[pred6] = nums[pred6] + 1 
+#         nums[pred7] = nums[pred7] + 1 
 
-         prediction = np.argmax(nums)
+#         prediction = np.argmax(nums)
 
-         file.write(str(idx + 1))
-         file.write(",")
-         file.write(str(prediction))
-         file.write("\n")
+#         file.write(str(idx + 1))
+#         file.write(",")
+#         file.write(str(prediction))
+#         file.write("\n")
---
+++ DigitRecognizer/DigitRecognizer/model3.py
@@ -0,0 +1,241 @@
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from random import randint
+import numpy as np
+
+image_size = 28     #28x28 images
+num_channels = 1    #Not RGB, just single values for each pixel
+num_labels = 10     #0-9
+
+patch_size_3 = 3
+patch_size_5 = 5
+patch_size_7 = 7
+batch_size = 128
+
+depth = 16
+num_hidden = 64
+
+#Load data  
+data = pd.read_csv("../input/train.csv");
+test_data = pd.read_csv("../input/test.csv");
+test_data = test_data.as_matrix().reshape((-1, image_size, image_size, num_channels)).astype(np.float32)
+
+
+#Partition into train/test sets
+images = data.iloc[0:5000,1:]
+labels = data.iloc[0:5000,:1]
+train_images, test_images, train_labels, test_labels = train_test_split(images, labels, train_size=0.99, random_state=0)
+
+#Cleanup
+del data
+del images
+del labels
+
+
+def reformat(dataset, labels):
+  dataset = dataset.reshape((-1, image_size, image_size, num_channels)).astype(np.float32)
+  labels = (np.arange(num_labels) == labels[:,None]).astype(np.float32)
+  labels = np.reshape(labels, (len(labels), num_labels))
+  return dataset, labels
+
+print("Shapes Before Reformat")
+print("train_images: ", train_images.shape)
+print("train_labels: ", train_labels.shape)
+print("test_images: ", test_images.shape)
+print("test_labels: ", test_labels.shape)
+
+#Convert to numpy arrays for tensorflow
+train_images, train_labels = reformat(train_images.as_matrix(), train_labels.as_matrix())
+test_images, test_labels = reformat(test_images.as_matrix(), test_labels.as_matrix())
+
+
+def ConvNet(model_save_path):
+
+    graph = tf.Graph()
+    with graph.as_default():
+
+      # Input data.
+      tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, image_size, image_size, num_channels))
+      tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+      tf_test_dataset = tf.constant(test_images, dtype=tf.float32)
+      
+      layer1_weights = tf.get_variable("layer1_weights", [patch_size_7, patch_size_7, num_channels, depth], initializer=tf.contrib.layers.xavier_initializer())
+      layer1_biases = tf.get_variable("layer1_biases",[depth], initializer=tf.contrib.layers.xavier_initializer())
+      layer2_weights = tf.get_variable("layer2_weights", [patch_size_5, patch_size_5, depth, depth], initializer=tf.contrib.layers.xavier_initializer())
+      layer2_biases = tf.get_variable("layer2_biases",[depth], initializer=tf.contrib.layers.xavier_initializer())
+      layer3_weights = tf.get_variable("layer3_weights", [patch_size_5, patch_size_5, depth, depth * 4], initializer=tf.contrib.layers.xavier_initializer())
+      layer3_biases = tf.get_variable("layer3_biases",[depth * 4], initializer=tf.contrib.layers.xavier_initializer())
+      layer4_weights = tf.get_variable("layer4_weights", [patch_size_5, patch_size_5, depth * 4, depth * 4], initializer=tf.contrib.layers.xavier_initializer())
+      layer4_biases = tf.get_variable("layer4_biases",[depth * 4], initializer=tf.contrib.layers.xavier_initializer())
+      layer5_weights = tf.get_variable("layer5_weights", [patch_size_5, patch_size_5, depth * 4, depth * 8], initializer=tf.contrib.layers.xavier_initializer())
+      layer5_biases = tf.get_variable("layer5_biases",[depth * 8], initializer=tf.contrib.layers.xavier_initializer())
+      layer6_weights = tf.get_variable("layer6_weights", [patch_size_3, patch_size_3, depth * 8, depth * 8], initializer=tf.contrib.layers.xavier_initializer())
+      layer6_biases = tf.get_variable("layer6_biases", [depth * 8], initializer=tf.contrib.layers.xavier_initializer())
+      fc = 2048
+      layer7_weights = tf.get_variable("layer7_weights", [fc, fc], initializer=tf.contrib.layers.xavier_initializer())
+      layer7_biases = tf.get_variable("layer7_biases", [fc], initializer=tf.contrib.layers.xavier_initializer())
+      
+      fc = 2048
+      layer8_weights = tf.get_variable("layer8_weights", [fc, num_labels], initializer=tf.contrib.layers.xavier_initializer())
+      layer8_biases = tf.get_variable("layer8_biases", [num_labels], initializer=tf.contrib.layers.xavier_initializer())
+
+      # Model
+      def model(data, keep_prob):
+        conv = tf.nn.conv2d(data, layer1_weights, [1, 1, 1, 1], padding='SAME')
+        hidden = tf.nn.relu(conv + layer1_biases)
+
+        conv = tf.nn.conv2d(hidden, layer2_weights, [1, 1, 1, 1], padding='SAME')
+        hidden = tf.nn.relu(conv + layer2_biases)
+        pool_1 = tf.nn.max_pool(hidden, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+        #Conv->Relu->Conv-Relu->Pool
+        conv = tf.nn.conv2d(pool_1, layer3_weights, [1, 1, 1, 1], padding='SAME')
+        hidden = tf.nn.relu(conv + layer3_biases)
+
+        conv = tf.nn.conv2d(hidden, layer4_weights, [1, 1, 1, 1], padding='SAME')
+        hidden = tf.nn.relu(conv + layer4_biases)
+        pool_1 = tf.nn.max_pool(hidden, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+        #Conv->Relu->Conv-Relu->Pool
+        conv = tf.nn.conv2d(pool_1, layer5_weights, [1, 1, 1, 1], padding='SAME')
+        hidden = tf.nn.relu(conv + layer5_biases)
+
+        conv = tf.nn.conv2d(hidden, layer6_weights, [1, 1, 1, 1], padding='SAME')
+        hidden = tf.nn.relu(conv + layer6_biases)
+        pool_1 = tf.nn.max_pool(hidden, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+        #Dropout -> Fully Connected -> Dropout -> Fully Connected
+        drop = tf.nn.dropout(pool_1, keep_prob)
+        shape = drop.get_shape().as_list()
+        reshape = tf.reshape(drop, [shape[0], shape[1] * shape[2] * shape[3]])
+        hidden = tf.matmul(reshape, layer7_weights) + layer7_biases 
+        drop = tf.nn.dropout(hidden, keep_prob)
+        return tf.matmul(drop, layer8_weights) + layer8_biases 
+
+      def accuracy(predictions, labels):
+        return 100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1)) / predictions.shape[0]
+      
+      # Training computation.
+      logits = model(tf_train_dataset, 0.5)
+      loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits))
+        
+      tf.summary.scalar("Loss", loss)
+
+      # Optimizer.
+      optimizer = tf.train.AdamOptimizer(0.0000035).minimize(loss)
+      
+      # Predictions for the training, validation, and test data.
+      train_prediction = tf.nn.softmax(logits)
+      test_prediction = tf.nn.softmax(model(tf_test_dataset, 1.0))
+
+      num_steps = 30001
+
+    with tf.Session(graph=graph) as session:
+      merged = tf.summary.merge_all()
+      writer = tf.summary.FileWriter('./train', session.graph)
+
+      tf.global_variables_initializer().run()
+      print('Initialized')
+      for step in range(num_steps):
+        offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+        batch_data = train_images[offset:(offset + batch_size), :, :, :]
+        batch_labels = train_labels[offset:(offset + batch_size), :]
+        feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
+
+        if (step % 250 == 0):
+          _, l, predictions, m = session.run([optimizer, loss, train_prediction, merged], feed_dict=feed_dict)
+          writer.add_summary(m, step)
+          print('Minibatch loss at step %d: %f' % (step, l))
+          print('Minibatch accuracy: %.1f%%' % accuracy(predictions, batch_labels))
+          print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
+        else:
+          #Don't pass in merged dictionary for better performance
+          _, l, predictions = session.run([optimizer, loss, train_prediction], feed_dict=feed_dict)
+
+
+      print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
+      
+      #Save session
+      saver = tf.train.Saver()
+      save_path = saver.save(session, model_save_path)
+
+
+
+def LoadAndRun():
+    tf.reset_default_graph()
+    graph = tf.Graph()
+
+    with graph.as_default():
+      # Input data.
+      tf_test_dataset = tf.constant(test_data, dtype=tf.float32)
+      
+      layer1_weights = tf.get_variable("layer1_weights", [patch_size_7, patch_size_7, num_channels, depth], initializer=tf.contrib.layers.xavier_initializer())
+      layer1_biases = tf.get_variable("layer1_biases",[depth], initializer=tf.contrib.layers.xavier_initializer())
+      layer2_weights = tf.get_variable("layer2_weights", [patch_size_5, patch_size_5, depth, depth], initializer=tf.contrib.layers.xavier_initializer())
+      layer2_biases = tf.get_variable("layer2_biases",[depth], initializer=tf.contrib.layers.xavier_initializer())
+      layer3_weights = tf.get_variable("layer3_weights", [patch_size_5, patch_size_5, depth, depth * 4], initializer=tf.contrib.layers.xavier_initializer())
+      layer3_biases = tf.get_variable("layer3_biases",[depth * 4], initializer=tf.contrib.layers.xavier_initializer())
+      layer4_weights = tf.get_variable("layer4_weights", [patch_size_5, patch_size_5, depth * 4, depth * 4], initializer=tf.contrib.layers.xavier_initializer())
+      layer4_biases = tf.get_variable("layer4_biases",[depth * 4], initializer=tf.contrib.layers.xavier_initializer())
+      layer5_weights = tf.get_variable("layer5_weights", [patch_size_5, patch_size_5, depth * 4, depth * 8], initializer=tf.contrib.layers.xavier_initializer())
+      layer5_biases = tf.get_variable("layer5_biases",[depth * 8], initializer=tf.contrib.layers.xavier_initializer())
+      layer6_weights = tf.get_variable("layer6_weights", [patch_size_3, patch_size_3, depth * 8, depth * 8], initializer=tf.contrib.layers.xavier_initializer())
+      layer6_biases = tf.get_variable("layer6_biases", [depth * 8], initializer=tf.contrib.layers.xavier_initializer())
+      fc = 2048
+      layer7_weights = tf.get_variable("layer7_weights", [fc, fc], initializer=tf.contrib.layers.xavier_initializer())
+      layer7_biases = tf.get_variable("layer7_biases", [fc], initializer=tf.contrib.layers.xavier_initializer())
+      
+      fc = 2048
+      layer8_weights = tf.get_variable("layer8_weights", [fc, num_labels], initializer=tf.contrib.layers.xavier_initializer())
+      layer8_biases = tf.get_variable("layer8_biases", [num_labels], initializer=tf.contrib.layers.xavier_initializer())
+
+      # Model
+      def model(data, keep_prob):
+        conv = tf.nn.conv2d(data, layer1_weights, [1, 1, 1, 1], padding='SAME')
+        hidden = tf.nn.relu(conv + layer1_biases)
+
+        conv = tf.nn.conv2d(hidden, layer2_weights, [1, 1, 1, 1], padding='SAME')
+        hidden = tf.nn.relu(conv + layer2_biases)
+        pool_1 = tf.nn.max_pool(hidden, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+        #Conv->Relu->Conv-Relu->Pool
+        conv = tf.nn.conv2d(pool_1, layer3_weights, [1, 1, 1, 1], padding='SAME')
+        hidden = tf.nn.relu(conv + layer3_biases)
+
+        conv = tf.nn.conv2d(hidden, layer4_weights, [1, 1, 1, 1], padding='SAME')
+        hidden = tf.nn.relu(conv + layer4_biases)
+        pool_1 = tf.nn.max_pool(hidden, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+        #Conv->Relu->Conv-Relu->Pool
+        conv = tf.nn.conv2d(pool_1, layer5_weights, [1, 1, 1, 1], padding='SAME')
+        hidden = tf.nn.relu(conv + layer5_biases)
+
+        conv = tf.nn.conv2d(hidden, layer6_weights, [1, 1, 1, 1], padding='SAME')
+        hidden = tf.nn.relu(conv + layer6_biases)
+        pool_1 = tf.nn.max_pool(hidden, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+        #Dropout -> Fully Connected -> Dropout -> Fully Connected
+        drop = tf.nn.dropout(pool_1, keep_prob)
+        shape = drop.get_shape().as_list()
+        reshape = tf.reshape(drop, [shape[0], shape[1] * shape[2] * shape[3]])
+        hidden = tf.matmul(reshape, layer7_weights) + layer7_biases 
+        drop = tf.nn.dropout(hidden, keep_prob)
+        return tf.matmul(drop, layer8_weights) + layer8_biases 
+
+
+      test_prediction = tf.nn.softmax(model(tf_test_dataset, 1.0))
+
+      with tf.Session(graph=graph) as session:
+        saver = tf.train.Saver()
+        saver.restore(session, model_save_path1)
+        print("Restored model 1")
+
+        x = test_prediction.eval();
+        results1 = np.argmax(x, 1)
+        print(results1)
+
+        return results1;
+
+      
