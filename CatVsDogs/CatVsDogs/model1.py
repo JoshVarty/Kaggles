52b47b3c6b4c241bce14b0eb196c896525411e15
Josh Varty <joshvarty@gmail.com>
2017-09-10 11:19:38 -0400

    Deeper net

--- CatVsDogs/CatVsDogs/model1.py
+++ CatVsDogs/CatVsDogs/model1.py
@@ -103,7 +103,7 @@ def ConvNet(model_save_path):
 
     batch_size = 16
     patch_size_3 = 3
-    depth = 16
+    depth = 32
     graph = tf.Graph()
     with graph.as_default():
 
@@ -116,20 +116,22 @@ def ConvNet(model_save_path):
       layer1_biases = tf.get_variable("layer1_biases",[depth], initializer=tf.contrib.layers.xavier_initializer())
       layer2_weights = tf.get_variable("layer2_weights", [patch_size_3, patch_size_3, depth, depth], initializer=tf.contrib.layers.xavier_initializer())
       layer2_biases = tf.get_variable("layer2_biases",[depth], initializer=tf.contrib.layers.xavier_initializer())
+
       layer3_weights = tf.get_variable("layer3_weights", [patch_size_3, patch_size_3, depth, depth * 2], initializer=tf.contrib.layers.xavier_initializer())
       layer3_biases = tf.get_variable("layer3_biases",[depth * 2], initializer=tf.contrib.layers.xavier_initializer())
       layer4_weights = tf.get_variable("layer4_weights", [patch_size_3, patch_size_3, depth * 2, depth * 2], initializer=tf.contrib.layers.xavier_initializer())
       layer4_biases = tf.get_variable("layer4_biases",[depth * 2], initializer=tf.contrib.layers.xavier_initializer())
+
       layer5_weights = tf.get_variable("layer5_weights", [patch_size_3, patch_size_3, depth * 2, depth * 4], initializer=tf.contrib.layers.xavier_initializer())
       layer5_biases = tf.get_variable("layer5_biases",[depth * 4], initializer=tf.contrib.layers.xavier_initializer())
       layer6_weights = tf.get_variable("layer6_weights", [patch_size_3, patch_size_3, depth * 4, depth * 4], initializer=tf.contrib.layers.xavier_initializer())
       layer6_biases = tf.get_variable("layer6_biases", [depth * 4], initializer=tf.contrib.layers.xavier_initializer())
       
-      fc = 9216
-      layer7_weights = tf.get_variable("layer7_weights", [fc, fc], initializer=tf.contrib.layers.xavier_initializer())
-      layer7_biases = tf.get_variable("layer7_biases", [fc], initializer=tf.contrib.layers.xavier_initializer())
+      fc = 18432
+      layer7_weights = tf.get_variable("layer7_weights", [fc, 4096], initializer=tf.contrib.layers.xavier_initializer())
+      layer7_biases = tf.get_variable("layer7_biases", [4096], initializer=tf.contrib.layers.xavier_initializer())
       
-      layer8_weights = tf.get_variable("layer8_weights", [fc, num_labels], initializer=tf.contrib.layers.xavier_initializer())
+      layer8_weights = tf.get_variable("layer8_weights", [4096, num_labels], initializer=tf.contrib.layers.xavier_initializer())
       layer8_biases = tf.get_variable("layer8_biases", [num_labels], initializer=tf.contrib.layers.xavier_initializer())
 
       # Model
