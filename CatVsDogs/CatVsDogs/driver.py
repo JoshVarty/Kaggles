637e7b05d7a688474c376460d7984ee9e81bd93c
Josh Varty <joshvarty@gmail.com>
2017-09-12 13:17:20 -0400

    Save output to results.csv

--- CatVsDogs/CatVsDogs/driver.py
+++ CatVsDogs/CatVsDogs/driver.py
@@ -9,4 +9,20 @@ model_save_path6 = "model/model6.ckpt"
 model_save_path7 = "model/model7.ckpt"
 
 #Three of the first model
-model1.LoadAndRun(model_save_path1)
+results1 = model1.LoadAndRun(model_save_path1)
+
+
+with open("results/results.csv", 'w') as file:
+    file.write("ImageId,Label\n")
+    for idx in range(len(results1)):
+         pred1 = int(results1[idx])
+
+         nums = np.zeros(10)
+         nums[pred1] = nums[pred1] + 1 
+
+         prediction = np.argmax(nums)
+
+         file.write(str(idx + 1))
+         file.write(",")
+         file.write(str(prediction))
+         file.write("\n")
